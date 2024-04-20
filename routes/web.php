535644de4972c85d020c1ae78ddf99a10879fcdb
Sang Nguyen <sangnguyenplus@gmail.com>
2024-04-20 22:27:57 +0700

    Add permission

--- routes/web.php
+++ routes/web.php
@@ -7,12 +7,14 @@ use Botble\DataSynchronize\Http\Controllers\UploadController;
 use Illuminate\Support\Facades\Route;
 
 AdminHelper::registerRoutes(function () {
-    Route::get('tools/data-synchronize', [DataSynchronizeController::class, 'index'])
-        ->name('data-synchronize.tools.data-synchronize');
+    Route::group(['permission' => 'tools.data-synchronize'], function () {
+        Route::get('tools/data-synchronize', [DataSynchronizeController::class, 'index'])
+            ->name('data-synchronize.tools.data-synchronize');
 
-    Route::prefix('data-synchronize')->name('data-synchronize.')->group(function () {
-        Route::post('upload', [UploadController::class, '__invoke'])
-            ->middleware(DisableInDemoModeMiddleware::class)
-            ->name('upload');
+        Route::prefix('data-synchronize')->name('data-synchronize.')->group(function () {
+            Route::post('upload', [UploadController::class, '__invoke'])
+                ->middleware(DisableInDemoModeMiddleware::class)
+                ->name('upload');
+        });
     });
 });
